8d507d8615195469cb0f2abc272bdc4748a06132
kitsosenwedi <boyboykitso@gmail.com>
2024-02-02 11:34:24 +0200

    folder structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Service from './components/Service';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <About />
+    <Service />
+    <Contact />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
